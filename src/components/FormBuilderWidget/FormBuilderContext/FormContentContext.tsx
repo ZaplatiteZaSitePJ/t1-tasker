f3ba97fb416ddaa2e50684e97129d98c11400b1f
Artem M <rtemnovosioloff@yandex.ru>
2025-05-20 21:09:12 +0500

    add Context file

--- src/components/FormBuilderWidget/FormBuilderContext/FormContentContext.tsx
+++ src/components/FormBuilderWidget/FormBuilderContext/FormContentContext.tsx
@@ -1,4 +1,42 @@
+import { useContext, createContext, type ReactNode, type Dispatch, useReducer } from "react";
+import type { TextfieldInterface} from "../types/textfieldInterface";
+import type { SelectInterface } from "../types/selectInterface";
 
-import { createContext } from "react";
+type formElement = TextfieldInterface | SelectInterface 
 
-export const FormContentContext = createContext<null | object[]>(null)
+type formAction = {type: "ADD_ELEMENT", item: formElement}
+
+type ContextType = {
+    formContent: formElement[],
+    dispatch: Dispatch<formAction> 
+}
+
+const initialContent:formElement[] = [] 
+
+const FormContext = createContext<ContextType | null>(null)
+
+function contentReducer(state: formElement[], action: formAction): formElement[] {
+    switch (action.type) {
+        case "ADD_ELEMENT":
+            return [...state, action.item]
+        default:
+            return state;
+    }
+}
+
+export const FormProvider = ({children}: {children: ReactNode}) => {
+    const [state, dispatch] = useReducer(contentReducer, initialContent)
+
+    return (
+        <FormContext.Provider value={{ formContent: state, dispatch}}>
+            {children}
+        </FormContext.Provider>
+    );
+}
+
+export const useFormContext = () => {
+    const context = useContext(FormContext);
+    if (!context) throw new Error("useFormContext must be used within FormProvider");
+    return context;
+};
+
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -2,7 +2,7 @@ import { type FC } from "react";
 import styles from './MainPage.module.scss'
 import AddFormPanel from "./FormBuilderWidget/sub-components/AddFormPanel/AddFormPanel";
 import FormPanel from "./FormBuilderWidget/sub-components/FormPanel/FormPanel";
-import { FormContentContext } from "./FormBuilderWidget/FormBuilderContext/FormContentContext";
+import { FormProvider } from './FormBuilderWidget/FormBuilderContext/FormContentContext';
 
 const MainPage: FC = () => {
 
@@ -14,10 +14,10 @@ const MainPage: FC = () => {
             <main className={styles.main}>
                 <p className={styles.main__logo}>_design your own form_</p>
                 <section className={styles.main__formBuilderPlace}>
-                    <FormContentContext.Provider value={null}>
+                    <FormProvider>
                         <AddFormPanel />
                         <FormPanel />
-                    </FormContentContext.Provider>
+                    </FormProvider>
                 </section>
                 
             </main>

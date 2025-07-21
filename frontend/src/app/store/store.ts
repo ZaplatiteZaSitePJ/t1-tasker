a890f6da70249f3e08c2d02163c8d6dd331e2a65
Артем <nocoarteamc@gmail.com>
2025-07-21 21:48:08 +0500

    deleted localStorage integration cause useless

--- frontend/src/app/store/store.ts
+++ frontend/src/app/store/store.ts
@@ -1,16 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import taskReducer from "@features/Tasks/api/storeModel/tasks.slice"
+import taskReducer from "@features/Tasks/api/storeModel/tasks.slice";
 
 export const store = configureStore({
-    reducer: {
-        tasks: taskReducer
-    }
-})
-
-store.subscribe(() => {
-  const tasks = store.getState().tasks;
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+	reducer: {
+		tasks: taskReducer,
+	},
 });
 
-export type AppState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+store.subscribe(() => {
+	const tasks = store.getState().tasks;
+});
+
+export type AppState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
